0ee2f804fec5e2e033e1eefd43d5df2052748f0a
Yebin <yebin2821@gmail.com>
2023-05-17 19:25:42 +0900

    signin

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Header from "../components/Header";
+import background from "./../assets/loginImage.png";
+import Form from "react-bootstrap/Form";
 
 const SignIn = () => {
   return (
     <div style={{ backgroundColor: "#00632d", height: "100vh" }}>
-      <Header />
       <div
         style={{
           backgroundColor: "white",
@@ -13,8 +13,50 @@ const SignIn = () => {
           transform: "translateX(-50%)",
           width: "95%",
           height: "95%",
+          paddingTop: "55px",
+          display: "flex",
         }}
-      ></div>
+      >
+        <div
+          style={{
+            width: "50%",
+            position: "relative",
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div style={{ width: "50%" }}>
+            <span>Sign in</span>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control
+                  type="text"
+                  placeholder="Please add you ID here"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3 mt-3" controlId="formBasicPassword">
+                <Form.Control
+                  type="password"
+                  placeholder="Please add your password here"
+                />
+              </Form.Group>
+            </Form>
+          </div>
+        </div>
+        <div
+          style={{
+            backgroundImage: `url(${background})`,
+            width: "50%",
+            position: "relative",
+            height: "100%",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100% 100%",
+          }}
+        ></div>
+      </div>
     </div>
   );
 };
